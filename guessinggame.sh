1e54a49fda7a5ec50ec5c18a4176709d5da11aad
realkhalil <163490434+realkhalil@users.noreply.github.com>
2024-11-24 23:23:23 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
+
+# Fonction pour obtenir le nombre de fichiers dans le répertoire courant
+function count_files {
+    echo $(ls -1 | wc -l)
+}
+
+# Nombre correct de fichiers
+correct_count=$(count_files)
+user_guess=-1
+
+echo "Bienvenue dans le jeu de devinette !"
+echo "Combien de fichiers se trouvent dans le répertoire actuel ?"
+
+# Boucle pour demander à l'utilisateur jusqu'à ce qu'il devine correctement
+while [[ $user_guess -ne $correct_count ]]; do
+    read -p "Votre estimation : " user_guess
+    if [[ $user_guess -lt $correct_count ]]; then
+        echo "C'est trop bas ! Essayez encore."
+    elif [[ $user_guess -gt $correct_count ]]; then
+        echo "C'est trop haut ! Essayez encore."
+    fi
+done
+
+echo "Félicitations ! Vous avez trouvé le bon nombre de fichiers : $correct_count"
 
